584fe207f61052d931c18c377cb10689cbd3079a
0obstacleo0 <0obstacleo0@gmail.com>
2024-08-18 09:11:20 +0900

    [mod]バグ修正

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -18,7 +18,7 @@ router.put('/:id', async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     if (post.userId === req.body.userId) {
-      await Post.updateOne({
+      await post.updateOne({
         $set: req.body,
       });
       return res.status(200).json('投稿編集に成功しました');
@@ -80,9 +80,9 @@ router.put('/:id/like', async (req, res) => {
 });
 
 // タイムラインの投稿取得
-router.get('/timeline/all', async (req, res) => {
+router.get('/timeline/:userId', async (req, res) => {
   try {
-    const currentUser = await User.findById(req.body.userId);
+    const currentUser = await User.findById(req.params.userId);
     const userPosts = await Post.find({ userId: currentUser._id });
 
     const friendPosts = await Promise.all(
